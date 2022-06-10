544948c92b629fa4956493370d2c1f75a134c9d6
hwseo0705 <julieseo0705@gmail.com>
2022-06-10 18:03:22 +0900

    [06.10.22] Java Study Commit - Deque, PriorityQueue 2 lines lol

--- src/com/kh/practice/chap01_ploy/run/Run.java
+++ src/com/kh/practice/chap01_ploy/run/Run.java
@@ -2,8 +2,15 @@ package com.kh.practice.chap01_ploy.run;
 
 import com.kh.practice.chap01_ploy.view.LibraryMenu;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
 public class Run {
     public static void main(String[] args) {
         LibraryMenu lm = new LibraryMenu();
+
+        Deque<Integer> deque = new LinkedList<>();
+        PriorityQueue<Object> pq = new PriorityQueue<>();
     }
 }
