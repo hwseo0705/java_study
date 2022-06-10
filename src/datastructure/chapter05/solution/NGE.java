eaceb925866b36a2ff769425491dfb5f837a6aa6
hwseo0705 <julieseo0705@gmail.com>
2022-06-10 17:54:45 +0900

    [06.10.22] Java Study Commit - NGE FINAL

--- src/datastructure/chapter05/solution/NGE.java
+++ src/datastructure/chapter05/solution/NGE.java
@@ -1,34 +1,48 @@
 package datastructure.chapter05.solution;
 
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
+// 백준 17298 - 오큰수
 public class NGE {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] A = new int[N];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N]; // 수열 배열
+        int[] answer = new int[N]; // 정답 배열
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        sc.close();
 
-        Stack<Integer> stack = new Stack();
-        StringBuilder str = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0); // 처음에는 스택이 비어있으므로 항상 최초값을 추가
 
-        for (int i = 0; i < N-1; i++) {
-            stack.push(A[i]);
-            if (A[i] > A[i+1]) stack.push(A[i]);
-            else {
-                int nge = stack.pop();
-                while (!stack.isEmpty()) {
-                    str.append(nge);
-                }
+        // 3 5 2 7    || 5 7 7 -1
+        for (int i = 1; i < N; i++) {
+
+            while (!stack.isEmpty() && A[stack.peek()] < A[i]) {
+                answer[stack.pop()] = A[i]; // 정답 배열에 오큰수를 저장
             }
-
+            stack.push(i);
         }
 
-        System.out.println(str);
+        while (!stack.isEmpty()) {
+            answer[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int n : answer) {
+            sb.append(n).append(" ");
+        }
+        System.out.println(sb);
+
+
     }
 }
